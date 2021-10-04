98c4280cbb17678585c433bd7a67797edc955591
kwicher <krzysztof.wicher@diag.pl>
2021-10-04 11:31:33 +0200

    authentication and routing

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,3 +1,5 @@
+
+
 export default function EnterPage({}){
   return (
     <main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,21 @@
-
-import Link from 'next/link';
+import Link from "next/link";
+import toast from "react-hot-toast";
+import Loader from "../components/Loader";
 
 export default function Home() {
   return (
     <div>
+      <button onClick={() => toast.success('hello toast!')}>
+        Toast c
+      </button>
+      {/* <Loader show/>
+
       <Link prefetch={false} href={{
         pathname: '/[username]',
         query: { username: 'jeffd23'},
       }}>
         <a>Jeff's profile</a>
-      </Link>
+      </Link> */}
     </div>
-  )
+  );
 }
